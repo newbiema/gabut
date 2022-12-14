a9734fbd2910ac805c3ee8b2463e3c1fc17844d4
newbiema <113580904+newbiema@users.noreply.github.com>
2022-12-14 21:48:24 +0700

    Add files via upload
    INI HANYALAH BERCANDAAN SEMATA

---
+++ Gambar Titit .py
@@ -0,0 +1,330 @@
+import time
+time.sleep(1.5)
+print("\t\t\t\t=================================================================================================")
+time.sleep(1)
+print("\t\t\t\t===========================   *** Selamat Datang di Gambar Titit ***  ===========================")
+time.sleep(0.5)
+print("\t\t\t\t=================================================================================================")
+time.sleep(1)
+print('\n')
+
+
+print("\t Haii Pekenalkan Nama Saya Evan :)")
+time.sleep(1.2)
+print('\n')
+print("\t Saya adalah Mahasiswa Universitas Widyagama Malang jurusan Teknik Informatika")
+time.sleep(1.2)
+print('\n')
+
+
+print("\t Saya telah membuat sebuah program bahasa python sederhana yang berfungsi untuk\n\t menebak dan mendeskripsikan bagimana gambaran sebuah titit seseorang.")
+time.sleep(1.5)
+print('\n')
+
+print("\t Apakah Kamu tau titit ")
+while True:
+    titit=str(input("\t y/n:"))
+    print("\n")
+    if titit =="n":
+        print("\t Yaudah aku kasi tau yaaa")
+        time.sleep(1.5)
+        print("\n")
+        print("\t Titit adalah alat kelamin jantan")
+        time.sleep(1.5)
+        print("\n")
+        print("\t Titit merupakan organ eksternal, karena berada di luar ruang tubuh")
+        time.sleep(1.5)
+        print("\n")
+        print("\t Selengkapnya searching di google ya")
+        time.sleep(5)
+        print("\n")
+        
+        print("\t Nah karena kamu udah tau apa itu titit :)")
+        time.sleep(1.5)
+        print("\n")
+
+        print("\t Kita lanjut ke perkenalan ya")
+        time.sleep(1.5)
+        print("\n")
+
+
+        print("\t\tIsilah data dibawah ")
+        time.sleep(1.5)
+        print('\n')
+
+        nama=str(input("\t\t\tMasukkan Nama: "))
+        print("\t\t\tJenis Kelamin:")
+        while True: 
+            kelamin=input("\t\t\tl/p: ")
+            if kelamin =="l":
+                kelamin=" Si Gay"
+                break
+            elif kelamin =="p":
+                kelamin=" Si penyuka Titit"
+                break
+            else:
+                print("\t\t\tMohon masukkan input y atau n") 
+
+
+        alasan=str(input("\t\t\tAlasan Kamu Pengen Lihat Titit Orang: "))
+        time.sleep(2)
+        print("\n")
+
+
+        print("\t Hai "+ nama + kelamin)
+        time.sleep(4)
+        print("\t Alesan kamu pengen liat titit karena "+ alasan +" ya :v")
+        time.sleep(4)
+        print("\n")
+
+
+        print("\t Yaudah kalau gitu masukin data orang yang mau kamu lihat tititnya :)")
+        time.sleep(2)
+        print("\n")
+
+
+        a = str(input("\t\t\tMasukkan Nama Orang: "))
+        b = str(input("\t\t\tMasukkan Umur Orang: "))
+        print("\n")
+        time.sleep(2)
+
+
+        print("\t Apakah " + nama  +" setuju saya akan menggambar TITIT "+a)
+        time.sleep(0.5)
+        print("\n")
+        c =input("\t y/n: ")
+        while c == "n" :
+            print(nama+" KONTOLLLLLLLLLLLLLLLLL!!!!")
+        if c == "y" :
+            print("\tTunggu bentar ya "+ nama)
+            time.sleep(2)
+            print("\n")
+            print("\t\t\t\t\t\t\t\tBERSEDIAAAA!!!!!")
+            time.sleep(2)
+            print("\t\t\t\t\t\t\t\tSIAAAAAAPPPPPP")
+            time.sleep(2)
+            print("\t\t\t\t\t\t\t\tTADAAAAAAAAAAAAAAAA")
+            
+            print("\n")
+            from turtle import *
+            import random, os
+
+            os.system('mode con: cols=25 lines=8')
+            races = {
+                'White' : '#ffdbaf',
+                'Irish' : '#fff2e2',
+                'Black' : '#756046',
+                'African' :'#443421',
+                'Asian' : '#ffe8b2',
+                'Hispanic' : '#edb86f',
+                'Indian' : '#997138',
+                }
+
+            racename, racevalue = random.choice(list(races.items()))
+            bsize = random.randint(25,80)
+            girth = random.randint(25,bsize)
+            length = random.randint(150,450)
+            xoffset = 0 - (length/2)
+            print("Deskripsi Titit "+a)
+            print('Race:', racename)
+            print('Ball size:',bsize)
+            print('Girth:',girth)
+            print('Length:',length)
+            print("\n")
+
+            hideturtle()
+            setup(length + bsize + 225, bsize*4 + 205)
+            title('Titit '+str(a))
+            speed(15)
+
+            bgcolor('black')
+            color('black')
+            setx(xoffset)
+            color(racevalue) 
+
+
+            begin_fill()
+            circle(bsize)
+            circle(0 - bsize)
+            end_fill()
+
+            
+            begin_fill()
+            sety(0 - girth)
+            forward(length)
+            end_fill()
+
+
+            color('pink')
+            begin_fill()
+            circle(girth,180)
+            end_fill()
+
+            
+            color(racevalue)
+            begin_fill()
+            forward(length)
+            sety(0)
+
+
+            setpos(length + xoffset, 0 - girth)
+            setpos(length + xoffset, girth)
+            setpos(xoffset,0)
+            end_fill()
+            
+            done()
+            break
+
+            
+        
+    elif titit =="y":
+        print("\t Karena kamu tau apa itu titit :)")
+        time.sleep(1.5)
+        print("\n")
+
+        print("\t Kita lanjut ke perkenalan dulu ya")
+        time.sleep(1.5)
+        print("\n")
+
+
+        print("\t\tIsilah data dibawah ")
+        time.sleep(1.5)
+        print('\n')
+
+        nama=str(input("\t\t\tMasukkan Nama: "))
+        print("\t\t\tJenis Kelamin:")
+        while True: 
+            kelamin=input("\t\t\tl/p: ")
+            if kelamin =="l":
+                kelamin=" Si Gay"
+                break
+            elif kelamin =="p":
+                kelamin=" Si penyuka Titit"
+                break
+            else:
+                print("\t\t\tMohon masukkan input y atau n") 
+
+
+        alasan=str(input("\t\t\tAlasan Kamu Pengen Lihat Titit Orang: "))
+        time.sleep(2)
+        print("\n")
+
+
+        print("\t Hai "+ nama + kelamin)
+        time.sleep(2)
+        print("\n")
+        print("\t Alesan kamu pengen liat titit karena "+ alasan +" ya :v")
+        time.sleep(2)
+        print("\n")
+
+
+        print("\t Yaudah kalau gitu masukin data orang yang mau kamu lihat tititnya :)")
+        time.sleep(2)
+        print("\n")
+
+
+        a = str(input("\t\t\tMasukkan Nama Orang: "))
+        b = str(input("\t\t\tMasukkan Umur Orang: "))
+        print("\n")
+        time.sleep(2)
+
+
+        print("\t Apakah " + nama  +" setuju saya akan menggambar TITIT "+a)
+        time.sleep(0.5)
+        c =input("\t y/n: ")
+        while c == "n" :
+            print(nama+" KONTOLLLLLLLLLLLLLLLLL!!!!")
+        if c == "y" :
+            print("\tTunggu bentar ya "+ nama)
+            time.sleep(2)
+            print("\n")
+            print("\t\t\t\t\t\t\t\tBERSEDIAAAA!!!!!")
+            time.sleep(2)
+            print("\t\t\t\t\t\t\t\tSIAAAAAAPPPPPP")
+            time.sleep(2)
+            print("\t\t\t\t\t\t\t\tTADAAAAAAAAAAAAAAAA")
+            
+            print("\n")
+
+            from turtle import *
+            import random, os
+
+            os.system('mode con: cols=25 lines=8')
+            races = {
+                'White' : '#ffdbaf',
+                'Irish' : '#fff2e2',
+                'Black' : '#756046',
+                'African' :'#443421',
+                'Asian' : '#ffe8b2',
+                'Hispanic' : '#edb86f',
+                'Indian' : '#997138',
+                }
+
+            racename, racevalue = random.choice(list(races.items()))
+            bsize = random.randint(25,80)
+            girth = random.randint(25,bsize)
+            length = random.randint(150,450)
+            xoffset = 0 - (length/2)
+            print("Deskripsi Titit "+a)
+            print('Race:', racename)
+            print('Ball size:',bsize)
+            print('Girth:',girth)
+            print('Length:',length)
+            print("\n")
+
+            hideturtle()
+            setup(length + bsize + 225, bsize*4 + 205)
+            title('Titit '+str(a))
+            speed(15)
+
+            bgcolor('black')
+            color('black')
+            setx(xoffset)
+            color(racevalue) 
+
+
+            begin_fill()
+            circle(bsize)
+            circle(0 - bsize)
+            end_fill()
+
+            
+            begin_fill()
+            sety(0 - girth)
+            forward(length)
+            end_fill()
+
+
+            color('pink')
+            begin_fill()
+            circle(girth,180)
+            end_fill()
+
+            
+            color(racevalue)
+            begin_fill()
+            forward(length)
+            sety(0)
+
+
+            setpos(length + xoffset, 0 - girth)
+            setpos(length + xoffset, girth)
+            setpos(xoffset,0)
+            end_fill()
+            done()
+            break
+    else:
+        print("\t\t\tmasukkan inputan y atau n")
+
+
+
+
+
+
+
+
+
+
+
+      
+  
